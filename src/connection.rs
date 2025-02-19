ca361c9bbc25645b652faa22b7570ec097b12579
Jeffrey Igims <jjigims23@fb.com>
2025-02-18 21:32:21 -0500

    replace exact writes/reads with repeated calls

--- src/connection.rs
+++ src/connection.rs
@@ -17,27 +17,37 @@ impl Connection {
         Connection { stream }
     }
     pub fn write(&mut self, message: &str) -> Result<()> {
-        let mut buf = [0; 64];
+        let mut bytes_written = 0;
+        let len: u32 = message.len().try_into()?;
+        let buf = &len.to_le_bytes();
+        while bytes_written < HEADER_SIZE {
+            bytes_written += self.stream.write(&buf[bytes_written..])?;
+        }
 
-        // we should check query.len() <= 60
-        let len: u32 = message.len().try_into().unwrap();
-        buf[..HEADER_SIZE as usize].copy_from_slice(len.to_le_bytes().as_ref());
-        buf[HEADER_SIZE..HEADER_SIZE+message.len()].copy_from_slice(message.as_bytes());
-
-        self.stream.write_all(&buf)?;
-        self.stream.flush()?;
+        bytes_written = 0;
+        let buf = message.as_bytes();
+        while bytes_written < message.len() {
+            bytes_written += self.stream.write(&buf[bytes_written..])?;
+        }
+        
         Ok(())
     }
     pub fn read(&mut self) -> Result<String> {
         let mut response = [0; HEADER_SIZE];
-        self.stream.read_exact(&mut response)?;
+        let mut bytes_read = 0;
+        while bytes_read < HEADER_SIZE {
+            bytes_read += self.stream.read(&mut response[bytes_read..])?;
+        }
+
         let len = u32::from_le_bytes(response);
         println!("Attempting to read {:?} bytes...", len);
 
         let mut buffer = vec![0; len as usize];
-        self.stream.read_exact(&mut buffer)?;
-        let response = std::str::from_utf8(&buffer)?;
-        Ok(response.to_string())
+        bytes_read = 0;
+        while bytes_read < len as usize{
+            bytes_read += self.stream.read(&mut buffer[bytes_read..])?;
+        }
+        String::from_utf8(buffer).map_err(Into::into)
     }
     pub fn query(&mut self, query: &str) -> Result<String> {
         self.write(query)?;
