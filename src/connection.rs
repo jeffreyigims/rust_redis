a2167724113ec21644db5dd845557d680eaa6be2
Jeffrey Igims <jjigims23@fb.com>
2025-02-19 21:26:42 -0500

    echo server, non-blocking

--- src/connection.rs
+++ src/connection.rs
@@ -6,30 +6,58 @@ const HEADER_SIZE: usize = 4;
 
 pub struct Connection {
     stream: TcpStream,
+    
+    pub want_to_read: bool,
+    pub want_to_write: bool,
+
+    read_buffer: Vec<u8>,
+    write_buffer: Vec<u8>,
 }
 
 impl Connection {
     pub fn new<A: ToSocketAddrs>(address: A) -> Result<Self> {
         let stream = TcpStream::connect(address)?;
-        Ok(Connection { stream })
+        Ok(Connection { stream, want_to_read: true, want_to_write: false, read_buffer: Vec::new(), write_buffer: Vec::new() })
     }
     pub fn from_stream(stream: TcpStream) -> Self {
-        Connection { stream }
+        Connection { stream, want_to_read: true, want_to_write: false, read_buffer: Vec::new(), write_buffer: Vec::new() }
     }
-    pub fn write(&mut self, message: &str) -> Result<()> {
-        let mut bytes_written = 0;
-        let len: u32 = message.len().try_into()?;
-        let buf = &len.to_le_bytes();
-        while bytes_written < HEADER_SIZE {
-            bytes_written += self.stream.write(&buf[bytes_written..])?;
+    pub fn handle_read(&mut self) -> Result<Option<String>> {
+       let mut buf = [0; 32 * 1024];
+       let bytes_read = self.stream.read(&mut buf)?;
+
+       self.read_buffer.extend_from_slice(&buf[..bytes_read]);
+
+       if self.read_buffer.len() < HEADER_SIZE {
+           return Ok(None);
+       }
+
+       let message_len = u32::from_le_bytes(self.read_buffer[..HEADER_SIZE].try_into()?);
+        println!("Attempting to read {:?} bytes...", message_len);
+
+        let total_len = HEADER_SIZE + message_len as usize;
+        if self.read_buffer.len() < total_len {
+            return Ok(None);
         }
 
-        bytes_written = 0;
-        let buf = message.as_bytes();
-        while bytes_written < message.len() {
-            bytes_written += self.stream.write(&buf[bytes_written..])?;
+        let message = String::from_utf8(self.read_buffer[HEADER_SIZE..total_len].to_vec()).map_err(anyhow::Error::from)?;
+        Ok(Some(message))
+
+    }
+    pub fn handle_write(&mut self) -> Result<()> {
+        let bytes_written = self.stream.write(&self.write_buffer)?;
+        self.write_buffer.drain(..bytes_written);
+        if self.write_buffer.is_empty() {
+            self.want_to_write = false;
         }
-        
+        Ok(())
+    }
+    pub fn write(&mut self, message: &str) -> Result<()> {
+        self.want_to_read = false;
+        let len: u32 = message.len().try_into()?;
+        self.write_buffer.extend_from_slice(&len.to_le_bytes());
+        self.write_buffer.extend_from_slice(message.as_bytes());
+        self.want_to_write = true;
         Ok(())
     }
     pub fn read(&mut self) -> Result<String> {
@@ -50,7 +78,10 @@ impl Connection {
         String::from_utf8(buffer).map_err(Into::into)
     }
     pub fn query(&mut self, query: &str) -> Result<String> {
-        self.write(query)?;
+        let len: u32 = query.len().try_into()?;
+        self.stream.write(&len.to_le_bytes())?;
+        self.stream.write(query.as_bytes())?;
+        self.stream.flush()?;
         self.read()
     }
 }
--- src/server.rs
+++ src/server.rs
@@ -2,7 +2,10 @@ use std::net::TcpListener;
 use anyhow::Result;
 use clap::Parser;
 use std::net::IpAddr;
-use std::thread;
+use std::os::fd::{AsRawFd, RawFd};
+use libc::{poll, pollfd, POLLIN, POLLERR, POLLOUT};
+use std::io;
+use std::collections::HashMap;
 
 mod connection;
 use connection::Connection;
@@ -15,11 +18,9 @@ struct Args {
     port: u16,
 }
 
-fn handle_request(mut connection: Connection) -> Result<()> {
-    let message = connection.read()?;
+fn handle_request(connection: &mut Connection, message: &str) -> Result<()> {
     println!("Client said: {}", message);
-
-    connection.write("world!")?;
+    connection.write(message)?;
     Ok(())
 }
 fn main() -> Result<()> {
@@ -27,16 +28,85 @@ fn main() -> Result<()> {
     // define the address and port the server will listen on
     let addr: (IpAddr, u16) = ([127, 0, 0, 1].into(), args.port);
 
-    // create a soclket, bind to localhost:PORT, and listen on it 
+    // create a socket, bind to localhost:PORT, and listen on it 
     let listener = TcpListener::bind(&addr)?;
+    listener.set_nonblocking(true)?;
+    let fd: RawFd = listener.as_raw_fd();
+
+
+    let mut connections: HashMap<RawFd, Connection> = HashMap::new();
     loop {
-        match listener.accept() {
-            Ok((socket, _addr)) => {
-                thread::spawn(move || { handle_request(Connection::from_stream(socket)) });
-            },    
-            Err(e) => Err(anyhow::Error::from(e))?,
+        let mut fds: Vec<pollfd> = Vec::new();
+        fds.push(pollfd {
+            fd,
+            events: POLLIN,
+            revents: 0,
+        });
+        
+        for (&fd, conn) in connections.iter() {
+            fds.push(pollfd {
+                fd,
+                events: POLLERR,
+                revents: 0,
+            });
+
+            if conn.want_to_read {
+                fds.push(pollfd {
+                    fd,
+                    events: POLLIN,
+                    revents: 0,
+                });
+            }
+
+            if conn.want_to_write {
+                fds.push(pollfd {
+                    fd,
+                    events: POLLOUT,
+                    revents: 0,
+                });
+            }
         }
 
-        // we don't need to close the socket connection since it's tied to the lifetime of `socket`
+        // this should be the only non-blocking call 
+        let result = unsafe { poll(fds.as_mut_ptr(), fds.len() as libc::nfds_t, -1) };
+        if result == -1 {
+            eprintln!("poll call failed: {}", io::Error::last_os_error());
+            return Err(anyhow::Error::from(io::Error::last_os_error()));
+        }
+
+        if fds[0].revents & POLLIN != 0 {
+            println!("TcpListener is ready to accept a connection.");
+            match listener.accept() {
+                Ok((socket, _addr)) => {
+                    let fd = socket.as_raw_fd();
+                    connections.insert(fd, Connection::from_stream(socket));
+                },    
+                Err(e) => Err(anyhow::Error::from(e))?,
+            }
+        }
+
+        for fd in &fds[1..] {
+            if fd.revents & POLLERR != 0 {
+                println!("Error on file descriptor: {}", fd.fd);
+                // socket will be closed when it goes out of scope
+                connections.remove(&fd.fd);
+                
+            }
+            if fd.revents & POLLIN != 0 {
+                let conn = connections.get_mut(&fd.fd).unwrap();
+                match conn.handle_read()?
+                {
+                    Some(message) => {
+                        handle_request(conn, &message)?;
+                    },
+                    None => {},
+                }
+            }
+            if fd.revents & POLLOUT != 0 {
+                connections.get_mut(&fd.fd).unwrap().handle_write()?;
+            }
+        }   
     }
+
+    // we don't need to close the socket connection since it's tied to the lifetime of `socket`
 }
